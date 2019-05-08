6de5be20c18dcee12668abe2ef733de2e039d615
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-05-08 09:21:56 +0200

    Fixed prependKhiva

--- .CI/azure-pipelines/prependKhiva.ts
+++ .CI/azure-pipelines/prependKhiva.ts
@@ -1,3 +1,4 @@
-import * as path from ''path'';
-import * as toolLib from ''azure-pipelines-tool-lib/tool'';
+import * as path from 'path';
+import * as toolLib from 'azure-pipelines-tool-lib/tool';
+
 toolLib.prependPath(path.join("C:\Program Files\Khiva\v0"));
